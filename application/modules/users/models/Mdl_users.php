4d756c486d2ab3610b5e353ef11fdc81460d45dc
niteshbca <singhniteshbca@gmail.com>
2015-09-22 18:40:06 +0530

    complete verify email - yet check for login remains
    status of account must be 1 to log on

--- application/modules/users/models/Mdl_users.php
+++ application/modules/users/models/Mdl_users.php
@@ -18,7 +18,23 @@ class Mdl_users extends CI_Model
     private $social_id;
     private $token;
     private $provider;
+    private $status;
 
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param mixed $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
     /**
      * @param mixed $user_id
      */
@@ -117,8 +133,7 @@ class Mdl_users extends CI_Model
 
             case 'token':{
                 $this->setToken(func_get_arg(1));
-                echo $this->getToken();
-                echo 'token set';
+
               break;
             }
 
@@ -412,25 +427,14 @@ class Mdl_users extends CI_Model
         return $this->provider;
     }
 
-    /**
-     * PHP 5 introduces a destructor concept similar to that of other object-oriented languages, such as C++.
-     * The destructor method will be called as soon as all references to a particular object are removed or
-     * when the object is explicitly destroyed or in any order in shutdown sequence.
-     *
-     * Like constructors, parent destructors will not be called implicitly by the engine.
-     * In order to run a parent destructor, one would have to explicitly call parent::__destruct() in the destructor body.
-     *
-     * Note: Destructors called during the script shutdown have HTTP headers already sent.
-     * The working directory in the script shutdown phase can be different with some SAPIs (e.g. Apache).
-     *
-     * Note: Attempting to throw an exception from a destructor (called in the time of script termination) causes a fatal error.
-     *
-     * @return void
-     * @link http://php.net/manual/en/language.oop5.decon.php
-     */
-    function __destruct()
-    {
-        // TODO: Implement __destruct() method.
-        echo 'destroyed model';
+    public function insertToken(){
+        return $this->db->where('hlu_users_id',$this->getUserId())->update('hlu_users',[
+           'hlu_users_token'=>$this->getToken()
+        ])?true:false;
+    }
+    public function verifyEmail(){
+        return $this->db->where('hlu_users_token',$this->getToken())->update('hlu_users',[
+        'hlu_users_status'=>'1'
+        ])?true:false;
     }
 }
